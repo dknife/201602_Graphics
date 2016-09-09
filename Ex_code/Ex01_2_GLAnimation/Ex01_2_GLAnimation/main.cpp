d823e96c5cf7ef35507179c68eb05fa82bfbdd15
Dknife <young.min.kang@gmail.com>
2016-09-09 12:35:02 +0900

    ex1_2

---
+++ Ex_code/Ex01_2_GLAnimation/Ex01_2_GLAnimation/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/glut.h>
+#include <math.h>
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPointSize(5);
+	glBegin(GL_POINTS);
+		static float x = -1.0;
+		float y = x*x;
+		glColor3f(0, y, 0);
+		glVertex3f(x, y, 0);
+		x += 0.0001;
+		if (x > 1.0) x -= 2.0;
+	glEnd();
+
+	glFlush();
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("I Love Graphics");
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+
+	// common state
+	glClearColor(1, 1, 1, 1);
+
+	// go into the loop
+	glutMainLoop();
+	return 1;
+}
