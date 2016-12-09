ab28b8f8eaac772d56b7a8cb8deafc867786ed13
Dknife <young.min.kang@gmail.com>
2016-12-09 09:44:44 +0900

    multiple textures

---
+++ Ex_code/Ex12_01_MultipleTextures/Ex12_01_MultipleTextures/main.cpp
@@ -0,0 +1,87 @@
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/glut.h>
+#include <math.h>
+#include "STBImage.h"
+
+unsigned char *myTex;
+int texWidth, texHeight, bitPerPixel;
+
+void CreateTexture(void) {
+	myTex = stbi_load("dirty.jpg", &texWidth, &texHeight, &bitPerPixel, 0);
+}
+
+void SetupTexture(void) {
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, 
+		GL_RGB, GL_UNSIGNED_BYTE, myTex);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glEnable(GL_TEXTURE_2D);
+}
+
+void drawQuad(void) {
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 1);
+	glVertex3f(-1, 1, 0);
+	glTexCoord2f(0, 0);
+	glVertex3f(-1, -1, 0);
+	glTexCoord2f(1, 0);
+	glVertex3f(1, -1, 0);
+	glTexCoord2f(1, 1);
+	glVertex3f(1, 1, 0);
+	glEnd();
+}
+
+void myDisplay() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, 1.0, 0.1, 100);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0,0,4, 0, 0, 0, 0, 1, 0);
+
+
+	glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+	glColor3f(1, 1, 1);
+
+	glPushMatrix();
+	glTranslatef(-1.0, 0, 0);
+	drawQuad();
+	glPopMatrix();
+	
+	glPushMatrix();
+	glTranslatef( 1.0, 0, 0);
+	drawQuad();
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+void init() {
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	CreateTexture();
+	SetupTexture();
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL);
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(512, 512);
+	glutCreateWindow("My Texture");
+
+	init();
+
+	glutDisplayFunc(myDisplay);
+	glutIdleFunc(myDisplay);
+
+	glutMainLoop();
+
+	return 0;
+}
