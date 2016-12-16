33a82fa6d1adf96ab56c3c2bb10ad22b90fe2f6e
Dknife <young.min.kang@gmail.com>
2016-12-16 13:09:30 +0900

    tt

--- Ex_code/FinalExam/FinalExam/main.cpp
+++ Ex_code/FinalExam/FinalExam/main.cpp
@@ -5,17 +5,81 @@
 
 #include <math.h>
 #include "STBImage.h"
+#include "mesh.h"
 
 #define NTEXTURES 2
 
+float arm1XAngle = 0;
+float arm2ZAngle = 0;
 
 GLuint tex[NTEXTURES];
+GLuint clothTex;
+
 unsigned char *myTex=NULL;
 int texWidth, texHeight, bitPerPixel;
 
+CMesh mesh1, mesh2;
+
+//light parameters
+GLfloat lit_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat lit_specular[] = { 1.0, 0.0, 0.0, 1.0 };
+GLfloat lit_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat lit_position0[] = { 0, 5, 0, 1 };
+GLfloat lit_position1[] = { 0, 5, 5, 1 };
+
+
+// material parameters
+GLfloat mat_diffuse[] = { 1.0, 1.0, 0.3, 1.0 };
+GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_ambient[] = { 0.0, 0.03, 0.0, 1.0 };
+
+void SetLighting(void) {
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, lit_ambient);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, lit_ambient);
+
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lit_diffuse);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, lit_diffuse);
+
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, 90);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, lit_specular);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, lit_specular);
+}
+
+void SetLightPosition(void) {
+	glLightfv(GL_LIGHT0, GL_POSITION, lit_position0);
+	glLightfv(GL_LIGHT1, GL_POSITION, lit_position1);
+}
+
+void keyboard(unsigned char c, int, int) {
+	switch (c) {
+	case 27: exit(0);
+	case 'w': arm1XAngle -= 1; break;
+	case 's': arm1XAngle += 1; break;
+	case 'a': arm2ZAngle += 1; break;
+	case 'd': arm2ZAngle -= 1; break;
+	case 'j': lit_position0[0] -= 0.1; lit_position1[0] -= 0.1; break;
+	case 'k': lit_position0[0] += 0.1; lit_position1[0] += 0.1; break;
+	}
+}
 
 void PrepareTextures(int n, GLuint *textures) {
 	glGenTextures(n, tex);
+	glGenTextures(1, &clothTex);
+
+	if (myTex) delete[] myTex;
+	glBindTexture(GL_TEXTURE_2D, clothTex);
+	myTex = stbi_load("clothTex.jpg", &texWidth, &texHeight, &bitPerPixel, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, myTex);
+	delete[] myTex; myTex = NULL;
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glEnable(GL_TEXTURE_2D);
 }
 
 
@@ -56,12 +120,14 @@ void myDisplay() {
 	gluPerspective(60, 1.0, 0.1, 100);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(2,2,4, 0, 0, 0, 0, 1, 0);
-
+	gluLookAt(6,5,8, 0, 5, 0, 0, 1, 0);
+	SetLightPosition();
 
 	glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
 	glColor3f(1, 1, 1);
 
+	glActiveTexture(GL_TEXTURE0);
+	glDisable(GL_TEXTURE_2D);
 	glPushMatrix();
 	glScalef(2, 0.5, 2);
 	glutSolidCube(1.0);
@@ -70,27 +136,74 @@ void myDisplay() {
 	glTranslatef(0,0.25,0);
 	glutSolidSphere(0.3, 30, 30);
 
+	
+	glRotatef(arm1XAngle, 1, 0, 0);
+	glTranslatef(0.0, 0.5, 0.0);
+		glPushMatrix();
+		glScalef(0.2, 1.0, 0.2);
+		glutSolidCube(1.0);
+		glPopMatrix();
+	glTranslatef(0.0, 0.5, 0.0);
+	glutSolidSphere(0.3, 30, 30);
+
+	glRotatef(arm2ZAngle, 0, 0, 1);
+	glTranslatef(0.0, 0.5, 0.0);
 	glPushMatrix();
-	glScalef(0.2, 2.0, 0.2);
+	glScalef(0.2, 1.0, 0.2);
+	glutSolidCube(1.0);
+	glPopMatrix();
+
+	glTranslatef(0, 0.5, 0);
+	glPushMatrix();
+	glScalef(2, 0.1, 2);
 	glutSolidCube(1.0);
 	glPopMatrix();
 
 
+
+
 	// 자동 텍스처 생성
 	glActiveTexture(GL_TEXTURE0);
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
+	glScalef(0.1, 0.1, 0.1);
+	glBindTexture(GL_TEXTURE_2D, tex[0]);
 	glEnable(GL_TEXTURE_GEN_S); glEnable(GL_TEXTURE_GEN_T);
-	glTexGenf(GL_S, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
-	glTexGenf(GL_T, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
+	glTexGenf(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+	glTexGenf(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+	glMatrixMode(GL_MODELVIEW);
 
 	glActiveTexture(GL_TEXTURE1);
 	glEnable(GL_TEXTURE_GEN_S); glEnable(GL_TEXTURE_GEN_T);
 	glTexGenf(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
 	glTexGenf(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
 
+	glPushMatrix();
+	glScalef(0.1, 0.1, 0.1);
+	glActiveTexture(GL_TEXTURE0);
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
+	glScalef(0.1, 0.1, 0.1);
+	glBindTexture(GL_TEXTURE_2D, clothTex);
+	glEnable(GL_TEXTURE_2D);
+	glMatrixMode(GL_MODELVIEW);
+	glActiveTexture(GL_TEXTURE1);
+	glDisable(GL_TEXTURE_2D);
+	mesh2.show();
 
-	static float angle = 0.0;
-	glRotatef(angle, 1, 1, 1); angle += 0.5;
-	//glutSolidTeapot(1.0);
+	glActiveTexture(GL_TEXTURE0);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, tex[0]);
+	glActiveTexture(GL_TEXTURE1);
+	glEnable(GL_TEXTURE_2D);
+	mesh1.show();
+	
+	glPopMatrix();
+
+
+
+
+
 	
 	glutSwapBuffers();
 }
@@ -101,16 +214,21 @@ void init() {
 	PrepareTextures(NTEXTURES, tex);
 	// stage 0
 	glActiveTexture(GL_TEXTURE0);
-	SetTexture(0, "robot.jpg");
+	SetTexture(0, "dirty.jpg");
 
 	// stage 1
 	glActiveTexture(GL_TEXTURE1);
-	SetTexture(1, "dirty.jpg");
+	SetTexture(1, "spheremap.jpg");
 	
-	
+	mesh1.read("complex.sms");
+	mesh2.read("cloth.sms");
+
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
 	glEnable(GL_DEPTH_TEST);
+
+	SetLighting();
 }
 
 int main(int argc, char **argv) {
@@ -125,6 +243,7 @@ int main(int argc, char **argv) {
 
 	glutDisplayFunc(myDisplay);
 	glutIdleFunc(myDisplay);
+	glutKeyboardFunc(keyboard);
 
 	glutMainLoop();
 
---
+++ Ex_code/FinalExam/FinalExam/mesh.cpp
@@ -0,0 +1,107 @@
+#include "mesh.h"
+
+CMesh::CMesh() {
+	verts = NULL;
+	tris = NULL;
+	norms = NULL;
+}
+
+CMesh::~CMesh() {
+	if (verts) delete[] verts;
+	if (tris) delete[] tris;
+	if (norms) delete[] norms;
+}
+
+void CMesh::computeNormals() {
+	for (int i = 0; i < nF; i++) {
+
+		// current face = tris[i]
+		// three vertices : tris[i].v0, v1, v2
+		int v0 = tris[i].v0;
+		int v1 = tris[i].v1;
+		int v2 = tris[i].v2;
+		vertex normal = computeNormal(verts[v0], verts[v1], verts[v2]);
+		norms[v0].x += normal.x;
+		norms[v0].y += normal.y;
+		norms[v0].z += normal.z;
+		norms[v1].x += normal.x;
+		norms[v1].y += normal.y;
+		norms[v1].z += normal.z;
+		norms[v2].x += normal.x;
+		norms[v2].y += normal.y;
+		norms[v2].z += normal.z;
+	}
+
+	for (int i = 0; i < nV; i++) {
+		float len = sqrt(norms[i].x*norms[i].x +
+			norms[i].y*norms[i].y +
+			norms[i].z*norms[i].z);
+		norms[i].x /= len;
+		norms[i].y /= len;
+		norms[i].z /= len;
+	}
+}
+
+vertex CMesh::computeNormal(vertex a, vertex b, vertex c) {
+	vertex normal;
+	vertex u, v;
+	u.x = b.x - a.x; u.y = b.y - a.y; u.z = b.z - a.z;
+	v.x = c.x - a.x; v.y = c.y - a.y; v.z = c.z - a.z;
+	normal.x = u.y * v.z - u.z * v.y;
+	normal.y = u.x * v.z - u.z * v.x;
+	normal.z = u.x * v.y - u.y * v.x;
+
+	return normal;
+}
+
+void CMesh::read(char *fname) {
+	FILE *f;
+	fopen_s(&f, fname, "r");
+	if (!f) { printf("file not found\n"); return; }
+
+	fscanf(f, "%d", &nV);
+	if (verts) delete[] verts;
+	verts = new vertex[nV];
+	if (norms) delete[] norms;
+	norms = new vertex[nV];
+	for (int i = 0; i < nV; i++) {
+		fscanf(f, "%f", &verts[i].x);
+		fscanf(f, "%f", &verts[i].y);
+		fscanf(f, "%f", &verts[i].z);
+		norms[i].x = norms[i].y = norms[i].z = 0.0;
+	}
+	fscanf(f, "%d", &nF);
+	if (tris) delete[] tris;
+	tris = new triangle[nF];
+	for (int i = 0; i < nF; i++) {
+		fscanf(f, "%d", &tris[i].v0);
+		fscanf(f, "%d", &tris[i].v1);
+		fscanf(f, "%d", &tris[i].v2);
+	}
+
+	computeNormals();
+}
+
+void CMesh::show() {
+
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < nF; i++) {
+
+		// current face = tris[i]
+		// three vertices : tris[i].v0, v1, v2
+		int v0 = tris[i].v0;
+		int v1 = tris[i].v1;
+		int v2 = tris[i].v2;
+
+		glNormal3f(norms[v0].x, norms[v0].y, norms[v0].z);
+		glVertex3f(verts[v0].x, verts[v0].y, verts[v0].z);
+		glNormal3f(norms[v1].x, norms[v1].y, norms[v1].z);
+		glVertex3f(verts[v1].x, verts[v1].y, verts[v1].z);
+		glNormal3f(norms[v2].x, norms[v2].y, norms[v2].z);
+		glVertex3f(verts[v2].x, verts[v2].y, verts[v2].z);
+
+
+	}
+	glEnd();
+
+}
