f6e3f6a900611e9f95a5870bffe8273e4b613d53
Dknife <young.min.kang@gmail.com>
2016-12-16 10:52:10 +0900

    T

--- Ex_code/Ex12_02_multitexture/Ex12_02_multitexture/main.cpp
+++ Ex_code/Ex12_02_multitexture/Ex12_02_multitexture/main.cpp
@@ -16,46 +16,22 @@ int texWidth, texHeight, bitPerPixel;
 
 void PrepareTextures(int n, GLuint *textures) {
 	glGenTextures(n, tex);
-	glEnable(GL_TEXTURE_2D);	
 }
 
 
-GLuint LoadTexture(const char *filename) {
-	unsigned char *image;
-
-	int width, height, bPerPix;
-	image = stbi_load(filename, &width, &height, &bPerPix, 0);
-
-	GLuint tex;
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_2D, tex);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-		GL_UNSIGNED_BYTE, image);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glEnable(GL_TEXTURE_2D);
-
-	return tex;
-}
-
-/*
 void SetTexture(int idx, char *fname) {
 	
 	if (myTex) delete[] myTex;
 	glBindTexture(GL_TEXTURE_2D, tex[idx]);
-	myTex = stbi_load(fname, &texWidth, &texHeight, &bitPerPixel, 0);
-	glActiveTexture(GL_TEXTURE0+idx);
+	myTex = stbi_load(fname, &texWidth, &texHeight, &bitPerPixel, 0);	
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, myTex);
 	delete[] myTex; myTex = NULL;
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glBindTexture(GL_TEXTURE_2D, tex[idx]);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);	
 	glEnable(GL_TEXTURE_2D);
-}*/
+}
 	
 void drawQuad(void) {
 	glBegin(GL_QUADS);
@@ -86,8 +62,8 @@ void myDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
 	glColor3f(1, 1, 1);
 
-	glBindTexture(GL_TEXTURE_2D, tex[0]);
-	drawQuad();
+
+	glutSolidTeapot(1.0);
 	
 	glutSwapBuffers();
 }
@@ -95,15 +71,18 @@ void myDisplay() {
 void init() {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	
+	PrepareTextures(NTEXTURES, tex);
 	// stage 0
 	glActiveTexture(GL_TEXTURE0);
-	tex[0] = LoadTexture("dirty.jpg");
+	SetTexture(0, "robot.jpg");
 
 	// stage 1
 	glActiveTexture(GL_TEXTURE1);
-	tex[1] = LoadTexture("robot.jpg");
+	SetTexture(1, "dirty.jpg");
 	
 	
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
 }
 
